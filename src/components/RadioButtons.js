fd55dd5d0d56d2ed607350d17a0fe92b37c1373a
nickyu <nick.lai@oakda.io>
2020-12-09 18:20:58 +0800

    add radio dot js

---
+++ src/components/RadioButtons.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Field, ErrorMessage } from 'formik'
+import TextError from './TextError'
+
+function RadioButtons(props) {
+  const { label, name, options, ...rest } = props
+  return (
+    <div className='form-control'>
+      <label>{label}</label>
+      <Field name={name} {...rest}>
+        {
+          ({ field }) => {
+            console.log('Field', field)
+            return options.map(option => {
+              return (
+                <React.Fragment key={option.key}>
+                  <input
+                    type='radio'
+                    id={option.value}
+                    {...field}
+                    value={option.value}
+                    checked={field.value === option.value}
+                  />
+                  <label htmlFor={option.value}>{option.key}</label>
+                </React.Fragment>
+              )
+            })
+          }
+        }
+      </Field>
+      <ErrorMessage name={name} component={TextError} />
+    </div>
+  );
+}
+
+export default RadioButtons;
