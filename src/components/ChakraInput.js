cb69cb2d03564f3e6b47310aab59583e6c57fe5f
nickyu <nick.lai@oakda.io>
2020-12-10 14:31:21 +0800

    add chakra ui and use to write input.js

---
+++ src/components/ChakraInput.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Field } from 'formik'
+import { Input, FormControl, FormLabel, FormErrorMessage } from "@chakra-ui/react"
+
+function ChakraInput(props) {
+  const { label, name, ...rest } = props
+
+  return (
+    <Field name={name}>
+      {
+        ({ field, form }) => {
+          return <FormControl isInvalid={form.errors[name] && form.touched[name]}>
+            <FormLabel htmlFor={name}>{label}</FormLabel>
+            <Input id={name} {...rest} {...field} />
+            <FormErrorMessage>{form.errors[name]}</FormErrorMessage>
+          </FormControl>
+        }
+      }
+    </Field>
+  )
+}
+
+export default ChakraInput;
