c3e58f3460f3c146cf714fae9afcf974b3d08def
nickyu <nick.lai@oakda.io>
2020-12-10 18:04:34 +0800

    edit checkbox group js

--- src/components/CheckboxGroup.js
+++ src/components/CheckboxGroup.js
@@ -1,36 +1,34 @@
-import React from 'react';
+import React from 'react'
 import { Field, ErrorMessage } from 'formik'
 import TextError from './TextError'
 
-function CheckboxGroup(props) {
+function CheckboxGroup (props) {
   const { label, name, options, ...rest } = props
   return (
     <div className='form-control'>
       <label>{label}</label>
-      <Field name={name} {...rest}>
-        {
-          ({ field }) => {
-            console.log('Field', field)
-            return options.map(option => {
-              return (
-                <React.Fragment key={option.key}>
-                  <input
-                    type='checkbox'
-                    id={option.value}
-                    {...field}
-                    value={option.value}
-                    checked={field.value.includes(option.value)}
-                  />
-                  <label htmlFor={option.value}>{option.key}</label>
-                </React.Fragment>
-              )
-            })
-          }
-        }
+      <Field name={name}>
+        {({ field }) => {
+          return options.map(option => {
+            return (
+              <React.Fragment key={option.key}>
+                <input
+                  type='checkbox'
+                  id={option.value}
+                  {...field}
+                  {...rest}
+                  value={option.value}
+                  checked={field.value === option.value}
+                />
+                <label htmlFor={option.value}>{option.key}</label>
+              </React.Fragment>
+            )
+          })
+        }}
       </Field>
-      <ErrorMessage name={name} component={TextError} />
+      <ErrorMessage component={TextError} name={name} />
     </div>
-  );
+  )
 }
 
-export default CheckboxGroup;
+export default CheckboxGroup
